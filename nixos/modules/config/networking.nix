2c826c45366ecbfc22021d6f13d780a2ddd67263
Peter Hoeg <peter@speartail.com>
2016-05-16 20:27:56 +0800

    networkmanager module: support for dns and dhcp
    This adds support for specifying the dhcp and dns options to
    networkmanager. It just follows upstream's defaults if not specified so
    there are no changes in behaviour unless the user requests it.
    
    Especially the ```dns``` part is interesting as it allows us to do split
    lookups when connected to a VPN.
    
    Additional stuff
    
    - remove deprecated `hostname`
    - make loglevel configurable
    - do not log to auditd if auditing is disabled

--- nixos/modules/config/networking.nix
+++ nixos/modules/config/networking.nix
@@ -187,14 +187,13 @@ in
         "rpc".source = pkgs.glibc.out + "/etc/rpc";
 
         # /etc/hosts: Hostname-to-IP mappings.
-        "hosts".text =
-          ''
-            127.0.0.1 localhost
-            ${optionalString cfg.enableIPv6 ''
-              ::1 localhost
-            ''}
-            ${cfg.extraHosts}
-          '';
+        "hosts".text = ''
+          127.0.0.1 localhost
+          ${optionalString cfg.enableIPv6 ''
+            ::1 localhost
+          ''}
+          ${cfg.extraHosts}
+        '';
 
         # /etc/host.conf: resolver configuration file
         "host.conf".text = cfg.hostConf;
