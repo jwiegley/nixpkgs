438d7b6a4231c8d9199aa87c61c6645c0afa0dd8
Edward Tjörnhammar <ed@cflags.cc>
2017-05-25 17:33:19 +0200

    feedreader: init at 2.0.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2097,6 +2097,8 @@ with pkgs;
 
   fdm = callPackage ../tools/networking/fdm {};
 
+  feedreader = callPackage ../applications/networking/feedreaders/feedreader {};
+
   ferm = callPackage ../tools/networking/ferm { };
 
   fgallery = callPackage ../tools/graphics/fgallery {
