5899d5aeb5c8aaf0635950539703933fdb3b6a4f
Samuel Leathers <sam@appliedtrust.com>
2017-06-12 19:23:01 -0400

    changes requested in https://github.com/NixOS/nixpkgs/pull/26537

--- pkgs/applications/misc/lifelines/default.nix
+++ pkgs/applications/misc/lifelines/default.nix
@@ -1,12 +1,13 @@
-{ stdenv, fetchgit, autoconf, libtool, automake, pkgconfig, gettext, libiconv
-, bison, ncurses, perl }:
+{ stdenv, fetchFromGitHub, autoconf, libtool, automake, pkgconfig
+, gettext, libiconv, bison, ncurses, perl, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   version = "2017-06-12-unstable";
   name = "lifelines-${version}";
 
-  src = fetchgit {
-    url = "https://github.com/disassembler/lifelines";
+  src = fetchFromGitHub {
+    owner = "disassembler";
+    repo = "lifelines";
     rev = "71f3fa98b8939bd2217d5d76f83b5e5c596ab4de";
     sha256 = "02rb15gwxv5c2kw86yib1wr9xhvly7h9fi604kwmsvl6yf3hbihy";
   };
@@ -21,20 +22,14 @@ stdenv.mkDerivation rec {
     bison
     ncurses
     perl
-];
+  ];
+  nativeBuildInputs = [ autoreconfHook ];
 
-  preConfigure = ''
-    gettextize -f -c
-    aclocal
-    autoconf
-    autoheader
-    automake -a
-  '';
-
-  meta = {
+  meta = with stdenv.lib; {
     description = "Genealogy tool with ncurses interface";
-    homepage = "https://github.com/MarcNo/lifelines";
-    license = stdenv.lib.licenses.mit;
-    maintainers = [ stdenv.lib.maintainers.disassembler ];
+    homepage = "http://marcno.github.io/lifelines";
+    license = licenses.mit;
+    maintainers = with maintainers; [ disassembler ];
+    platforms = platforms.linux;
   };
 }
