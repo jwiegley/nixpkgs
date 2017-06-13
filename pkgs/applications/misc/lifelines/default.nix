b4fcbe3a29ec9f7e3d8552ab1fc1ca12e8754a1b
Samuel Leathers <sam@appliedtrust.com>
2017-06-13 10:58:29 -0400

    using nativeBuildInputs for build deps

--- pkgs/applications/misc/lifelines/default.nix
+++ pkgs/applications/misc/lifelines/default.nix
@@ -1,29 +1,23 @@
-{ stdenv, fetchFromGitHub, autoconf, libtool, automake, pkgconfig
-, gettext, libiconv, bison, ncurses, perl, autoreconfHook }:
+{ stdenv, fetchFromGitHub, gettext, libiconv, bison, ncurses, perl, autoreconfHook }:
 
 stdenv.mkDerivation rec {
   version = "2017-06-12";
   name = "lifelines-unstable-${version}";
 
   src = fetchFromGitHub {
-    owner = "MarcNo";
+    owner = "disassembler";
     repo = "lifelines";
-    rev = "61974641d9e2ca6ee808597d934b0f70eab69a56";
-    sha256 = "1nqn395pljwdz3rbanqcakzs00svqk53xjazfp66rqzia7wdh0s5";
+    rev = "5bf8bfbdf0462e038c31e89f5b5f255429cd3291";
+    sha256 = "183gmqax0sy858c15vq5q4qq9j9lid5xs5ah4whq4q2bc3xrwwa0";
   };
 
   buildInputs = [
-    autoconf
-    automake
-    libtool
-    pkgconfig
     gettext
     libiconv
-    bison
     ncurses
     perl
   ];
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [ autoreconfHook bison ];
 
   meta = with stdenv.lib; {
     description = "Genealogy tool with ncurses interface";
