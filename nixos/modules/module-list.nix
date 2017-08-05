61f83983fb0d81d04f51cef2fb0ee88c6f225ea6
Bas van Dijk <v.dijk.bas@gmail.com>
2017-08-05 14:01:52 +0200

    nixos: add the strongswan-swanctl service
    The strongswan-swanctl systemd service starts charon-systemd. This implements a IKE daemon
    very similar to charon, but it's specifically designed for use with systemd. It uses the
    systemd libraries for a native integration.
    
    Instead of using starter and an ipsec.conf based configuration, the daemon is directly
    managed by systemd and configured with the swanctl configuration backend.
    
    See: https://wiki.strongswan.org/projects/strongswan/wiki/Charon-systemd
    
    Note that the strongswan.conf and swantctl.conf configuration files are automatically
    generated based on NixOS options under services.strongswan-swanctl.strongswan and
    services.strongswan-swanctl.swanctl respectively.

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -523,6 +523,7 @@
   ./services/networking/ssh/lshd.nix
   ./services/networking/ssh/sshd.nix
   ./services/networking/strongswan.nix
+  ./services/networking/strongswan-swanctl/module.nix
   ./services/networking/supplicant.nix
   ./services/networking/supybot.nix
   ./services/networking/syncthing.nix
