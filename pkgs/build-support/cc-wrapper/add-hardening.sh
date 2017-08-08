1e6fc4dcf80343e97f5a87c8b62e395b4a5a522d
Charles Strahan <charles@cstrahan.com>
2017-08-08 15:15:44 -0400

    hardening: fix careless bugs
    I got a substitution backwards (used '+' instead of '-').
    
    Also, this now works under `set -u` (had to fix a couple unbound
    variable references).

--- pkgs/build-support/cc-wrapper/add-hardening.sh
+++ pkgs/build-support/cc-wrapper/add-hardening.sh
@@ -2,8 +2,8 @@ allHardeningFlags=(fortify stackprotector pie pic strictoverflow format relro bi
 hardeningCFlags=()
 hardeningLDFlags=()
 
-declare -A hardeningDisableMap
-declare -A hardeningEnableMap
+declare -A hardeningDisableMap=()
+declare -A hardeningEnableMap=()
 
 # Create table of unsupported flags for this toolchain.
 for flag in @hardening_unsupported_flags@; do
@@ -13,8 +13,8 @@ done
 # Intentionally word-split in case 'NIX_HARDENING_ENABLE' is defined in Nix. The
 # array expansion also prevents undefined variables from causing trouble with
 # `set -u`.
-for flag in ${NIX_HARDENING_ENABLE+}; do
-  if [[ -z "${hardeningDisableMap[$flag]}" ]]; then
+for flag in ${NIX_HARDENING_ENABLE-}; do
+  if [[ -z "${hardeningDisableMap[$flag]-}" ]]; then
     hardeningEnableMap[$flag]=1
   fi
 done
@@ -22,7 +22,7 @@ done
 if [[ -n "$NIX_DEBUG" ]]; then
   # Determine which flags were effectively disabled so we can report below.
   for flag in ${allHardeningFlags[@]}; do
-    if [[ -z "${hardeningEnableMap[$flag]}" ]]; then
+    if [[ -z "${hardeningEnableMap[$flag]-}" ]]; then
       hardeningDisableMap[$flag]=1
     fi
   done
