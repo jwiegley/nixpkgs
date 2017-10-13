32a3917fa7b63176dea93712b5e8e92cd8d14188
Matthieu Coudron <mattator@gmail.com>
2017-10-13 16:13:28 +0900

    alot: 0.5.1 to 0.6.0
    - fixed thanks to Sarah Brofeldt
    - added gnupg to checkInputs
    - generate manpage
    - move alot.desktop file to $out/share/applications
    - disabled tests as it has failures I could not fix

--- pkgs/development/python-modules/gpg/default.nix
+++ pkgs/development/python-modules/gpg/default.nix
@@ -1,66 +1,40 @@
-{ stdenv, pkgs, pythonPackages
-, fetchPypi
+{ stdenv, buildPythonPackage
+, fetchPypi, libgpgerror, swig2
+, gpgme
 , withManpage ? true }:
 
-with pythonPackages;
-
 buildPythonPackage rec {
-    version = "1.8.0";
-    name = "gpg-${version}";
-    pname = "gpg";
+  name = "gpg-${gpgme.version}";
+  pname = "gpg";
 
-    disabled = isPy3k;
+  src = gpgme.src;
 
-        src = fetchPypi {
-          inherit pname version;
-          sha256 = "1x74i6q713c0bckls7rdm8kgsmllf9qvy9x62jghszlhgjkyh9nd";
-        };
+  # it could also run --cflags instead
+  preConfigure = ''
+    ./configure --enable-languages=python
+    cd ${gpgme.pythonSourceRoot}
+    substituteInPlace setup.py \
+      --replace \
+      'gpg_error_prefix = getconfig("prefix", config=gpg_error_config)[0]' \
+      'gpg_error_prefix = "${libgpgerror.dev}"' \
+      --replace \
+      'gpgme_h = ""' \
+      'gpgme_h = "${gpgme.dev}/include/gpgme.h"'
+  '';
 
-    # src = pkgs.fetchFromGitHub {
-    #   owner = "pazz";
-    #   repo = "alot";
-    #   inherit version;
-    #   sha256 = "1x6in9y823zi0k6xk62nwjybkc3pp5n2ml79x4v9jy6fz2klf2mi";
-    # };
+  buildInputs = [
+    gpgme
+    libgpgerror
+    libgpgerror.dev
+    gpgme
+    swig2
+  ];
 
-    # postPatch = ''
-    #   substituteInPlace alot/defaults/alot.rc.spec \
-    #     --replace "themes_dir = string(default=None)" \
-    #               "themes_dir = string(default='$out/share/themes')"
-    # '';
-
-    #  # uses that one https://github.com/mitchellrj/python-pgp
-    # nativeBuildInputs = stdenv.lib.optionals withManpage [ sphinx python-gnupg ];
-    propagatedBuildInputs = [
-        pkgs.libgpgerror
-        pkgs.gpgme
-    #     urwid
-    #     urwidtrees
-    #     twisted
-    #     python_magic
-    #     configobj
-    #     pygpgme
-    #     mock
-      ];
-
-      # postBuild= stdenv.lib.optionalString withManpage "make -C docs man";
-      # ''
-      #   make man
-      # '';
-    # postInstall = ''
-    #   mkdir -p $out/share
-    #   cp -r extra/themes $out/share
-    #   wrapProgram $out/bin/alot \
-    #     --prefix LD_LIBRARY_PATH : '${pkgs.lib.makeLibraryPath [ pkgs.notmuch pkgs.file pkgs.gpgme ]}'
-    # '';
-
-    meta = {
-      homepage = https://www.gnupg.org;
-      description = "GPG bindings for python";
-      license = stdenv.licenses.lgpl2;
-      platforms = platforms.linux;
-      maintainers = with maintainers; [ teto ];
-    };
+  meta = with stdenv.lib; {
+    homepage = https://www.gnupg.org;
+    description = "GPG bindings for python";
+    license = licenses.lgpl2;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ teto ];
+  };
 }
-
-
