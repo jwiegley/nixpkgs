2a3055c04eae85e6788df9edb41545b3e8857fb2
Ben Gamari <ben@smart-cactus.org>
2017-10-15 13:07:05 -0400

    HACK: shadow: Don't depend upon glibcCross
    Things blow up.

--- pkgs/os-specific/linux/shadow/default.nix
+++ pkgs/os-specific/linux/shadow/default.nix
@@ -1,15 +1,10 @@
 { stdenv, fetchpatch, fetchFromGitHub, autoreconfHook, libxslt, libxml2
 , docbook_xml_dtd_412, docbook_xsl, gnome_doc_utils, flex, bison
-, pam ? null, glibcCross ? null
+, pam ? null
 , buildPlatform, hostPlatform
 }:
 
 let
-
-  glibc =
-    if hostPlatform != buildPlatform
-    then glibcCross
-    else assert stdenv ? glibc; stdenv.glibc;
 
   dots_in_usernames = fetchpatch {
     url = http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo-x86/sys-apps/shadow/files/shadow-4.1.3-dots-in-usernames.patch;
@@ -62,9 +57,9 @@ stdenv.mkDerivation rec {
 
   configureFlags = " --enable-man ";
 
-  preBuild = assert glibc != null;
+  preBuild =
     ''
-      substituteInPlace lib/nscd.c --replace /usr/sbin/nscd ${glibc.bin}/bin/nscd
+      substituteInPlace lib/nscd.c --replace /usr/sbin/nscd ${stdenv.lib.getBin stdenv.cc.libc}/bin/nscd
     '';
 
   postInstall =
