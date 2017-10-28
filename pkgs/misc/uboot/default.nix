69fcbd4e4ce54396994e0182de0f5fe15f1df876
Ben Gamari <ben@smart-cactus.org>
2017-10-28 15:50:36 -0400

    uboot: Make it build
    It seems that u-boot has a bug wherein it fails to link when configured with
    allnoconfig. The easiest workaround is just to configure such that the needed
    objects are built.

--- pkgs/misc/uboot/default.nix
+++ pkgs/misc/uboot/default.nix
@@ -4,12 +4,13 @@
 
 let
   buildUBoot = { targetPlatforms
-            , filesToInstall
-            , installDir ? "$out"
-            , defconfig
-            , extraMeta ? {}
-            , ... } @ args:
-           stdenv.mkDerivation (rec {
+               , filesToInstall
+               , installDir ? "$out"
+               , defconfig
+               , extraMeta ? {}
+               , otherConfig ? ""
+               , ... } @ args:
+            stdenv.mkDerivation (rec {
 
     name = "uboot-${defconfig}-${version}";
     version = "2017.09";
@@ -43,7 +44,10 @@ let
     hardeningDisable = [ "all" ];
 
     configurePhase = ''
-      make ${defconfig}
+      make $makeFlags ${defconfig}
+      # Apply otherConfig
+      echo "${otherConfig}" >> .config
+      make $makeFlags oldconfig
     '';
 
     installPhase = ''
@@ -81,6 +85,16 @@ in rec {
 
   ubootTools = buildUBoot rec {
     defconfig = "allnoconfig";
+    # This is necessary otherwise the build fails with undefined symbols at link-time.
+    # This is likely a bug in u-boot.
+    otherConfig = ''
+      CONFIG_FIT=y
+      CONFIG_FIT_SIGNATURE=y
+      CONFIG_FIT_ENABLE_SHA256_SUPPORT=y
+      CONFIG_FIT_VERBOSE=y
+      CONFIG_FIT_BEST_MATCH=y
+      CONFIG_SPL_RSA=y
+    '';
     installDir = "$out/bin";
     buildFlags = "tools NO_SDL=1";
     dontStrip = false;
