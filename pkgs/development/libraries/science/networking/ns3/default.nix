0ecb790c5caddaf59aa1fec3dcb779d2056498ff
Matthieu Coudron <mattator@gmail.com>
2017-11-09 03:57:12 +0900

    Addressing reviews

--- pkgs/development/libraries/science/networking/ns3/default.nix
+++ pkgs/development/libraries/science/networking/ns3/default.nix
@@ -12,44 +12,50 @@
 # texlive to get latexmk. building manual still fails though
 , dia, tetex ? null, ghostscript ? null, texlive ? null
 
-, withDoc ? false
+, enableDoxygen ? false
 , withManual ? false
-, withGsl ? false
+
+# to improve wifi precision
+, withGsl ? false, gsl ? null
+
+# e.g. "optimized" or "debug". If not set, use default one
+, build_profile ? null
 
 # --enable-examples
 , withExamples ? false
 
-# --enable-tests
-, withTests ? true
 , generateBindings ? false
 
 # All modules can be enabled by choosing 'all_modules'.
 # included here the DCE mandatory ones
 , modules ? [ "core" "network" "internet" "point-to-point"]
-, pkgs
+, gcc6
 , lib
 }:
 
 stdenv.mkDerivation rec {
+
   name = "ns-3.${version}";
   version = "27.0";
 
-  # the all in one fetches netanim etc.
-  # https://www.nsnam.org/release/ns-allinone-3.27.tar.bz2;
+  outputs = [ "out" "doc" ];
+
+  # the all in one https://www.nsnam.org/release/ns-allinone-3.27.tar.bz2;
+  # fetches everything (netanim etc), this package focuses ns3-core
   src = fetchFromGitHub {
     owner  = "nsnam";
     repo   = "ns-3-dev-git";
-    rev = "${name}";
+    rev    = "${name}";
     sha256 = "1w25qazsi4n35wzpxrfc1pdrid43gan3166c0bif196apras5phd";
   };
 
-  buildInputs = lib.optionals generateBindings [ pkgs.castxml pygccxml ]
-    ++ stdenv.lib.optional withDoc [ doxygen graphviz imagemagick python ]
+  buildInputs = lib.optionals generateBindings [ castxml pygccxml ]
+    ++ stdenv.lib.optional enableDoxygen [ doxygen graphviz imagemagick python ]
     ++ stdenv.lib.optional withManual [ python.pkgs.sphinx dia tetex ghostscript
    texlive.combined.scheme-medium ];
 
-  propagatedBuildInputs = with python.pythonPackages; with pkgs; [ gcc6 python ]
-    ++ stdenv.lib.optional withGsl [ gsl-bin libgsl2 libgsl ]
+  propagatedBuildInputs = with python.pythonPackages; [ gcc6 python ]
+    ++ optional withGsl gsl
     ;
 
   postPatch = ''
@@ -58,22 +64,23 @@ stdenv.mkDerivation rec {
 
   configureScript = "./waf configure";
 
-  configureFlags=[
+  configureFlags = with stdenv.lib; [
       "--enable-modules=${stdenv.lib.concatStringsSep "," modules}"
       ]
-      ++ stdenv.lib.optional withExamples " --enable-examples "
-      ++ stdenv.lib.optional withTests " --enable-tests "
+      ++ optional build_profile "--build-profile=${build_profile}"
+      ++ optional withExamples " --enable-examples "
+      ++ optional doCheck " --enable-tests "
       ;
 
   postBuild = with stdenv.lib; let flags = concatStringsSep ";" (
        optional generateBindings "./waf --apiscan=all "
-       ++ optional withDoc "./waf doxygen"
+       ++ optional enableDoxygen "./waf doxygen"
        ++ optional withManual "./waf sphinx"
       );
       in "${flags}"
     ;
 
-  doCheck = withTests || withExamples;
+  doCheck = true;
   checkPhase =  ''
     ./test.py
     '';
