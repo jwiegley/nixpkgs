703ad24b2633d19283db3f11c4d19f2df9d4196c
Vincent Demeester <vincent@sbr.pm>
2018-01-04 19:27:02 +0100

    gopkgs: init at 2017-12-29
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

---
+++ pkgs/development/tools/gopkgs/default.nix
@@ -0,0 +1,23 @@
+{ stdenv, lib, buildGoPackage, fetchgit }:
+
+buildGoPackage rec {
+  name = "gopkgs-${version}";
+  version = "unstable-2017-12-29";
+  rev = "b2ea2ecd37740e6ce0e020317d90c729aab4dc6d";
+
+  goPackagePath = "github.com/uudashr/gopkgs";
+  goDeps = ./deps.nix;
+
+  src = fetchgit {
+    inherit rev;
+    url = "https://github.com/uudashr/gopkgs";
+    sha256 = "1hwzxrf2h8xjbbx6l86mjpjh4csxxsy17zkh8h3qzncyfnsnczzg";
+  };
+
+  meta = {
+    description = "Tool to get list available Go packages.";
+    homepage = https://github.com/uudashr/gopkgs;
+    maintainers = with stdenv.lib.maintainers; [ vdemeester ];
+    license = stdenv.lib.licenses.mit;
+  };
+}
