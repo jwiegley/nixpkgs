c51657f999b2e1072113c326856856f0957c03d7
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-01-08 11:24:04 +1300

    libproxy: Fix libproxy for macOS

--- pkgs/development/libraries/libproxy/default.nix
+++ pkgs/development/libraries/libproxy/default.nix
@@ -1,5 +1,5 @@
-{ stdenv, lib, fetchFromGitHub, pkgconfig, cmake
-, dbus, networkmanager, spidermonkey_38, pcre, python2, python3 }:
+{ stdenv, lib, fetchFromGitHub, fetchpatch, pkgconfig, cmake
+, dbus, networkmanager, spidermonkey_38, pcre, python2, python3, zlib, darwin }:
 
 stdenv.mkDerivation rec {
   name = "libproxy-${version}";
@@ -12,22 +12,36 @@ stdenv.mkDerivation rec {
     sha256 = "10swd3x576pinx33iwsbd4h15fbh2snmfxzcmab4c56nb08qlbrs";
   };
 
+  patches = [
+    (fetchpatch {
+      url = https://patch-diff.githubusercontent.com/raw/libproxy/libproxy/pull/76.diff;
+      sha256 = "19yp29xww87mhgwdrr2ap0j1bj0g1sjbc36rh2n0klnn4ipm8xla";
+    })
+  ];
+
   outputs = [ "out" "dev" ]; # to deal with propagatedBuildInputs
 
   nativeBuildInputs = [ pkgconfig cmake ];
 
-  buildInputs = [ dbus networkmanager spidermonkey_38 pcre python2 python3 ];
+  buildInputs = [ dbus pcre python2 python3 ]
+   ++ lib.optionals stdenv.isLinux [ networkmanager spidermonkey_38 ]
+   ++ lib.optionals stdenv.isDarwin [
+     zlib
+     darwin.apple_sdk.frameworks.SystemConfiguration
+  ];
+
+  cmakeFlags = lib.optional stdenv.isLinux "-DWITH_MOZJS=ON"
+    ++ lib.optional stdenv.isDarwin "-DWITH_APPLE_WEBKIT=OFF";
 
   preConfigure = ''
     cmakeFlagsArray+=(
-      "-DWITH_MOZJS=ON"
       "-DPYTHON2_SITEPKG_DIR=$out/${python2.sitePackages}"
       "-DPYTHON3_SITEPKG_DIR=$out/${python3.sitePackages}"
     )
   '';
 
   meta = with stdenv.lib; {
-    platforms = platforms.linux;
+    platforms = platforms.unix;
     license = licenses.lgpl21;
     homepage = http://libproxy.github.io/libproxy/;
     description = "A library that provides automatic proxy configuration management";
