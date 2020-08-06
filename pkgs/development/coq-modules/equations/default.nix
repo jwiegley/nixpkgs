61c8bfd0fae50476f66469ec3271a78c6b5db436
John Wiegley <johnw@newartisans.com>
2020-08-06 16:06:23 -0700

    coqPackages_8_12.equations: 1.2.2 -> 1.2.3

--- pkgs/development/coq-modules/equations/default.nix
+++ pkgs/development/coq-modules/equations/default.nix
@@ -59,7 +59,7 @@ stdenv.mkDerivation rec {
     sha256 = param.sha256;
   };
 
-  buildInputs = with coq.ocamlPackages; [ ocaml camlp5 findlib coq ];
+  buildInputs = with coq.ocamlPackages; [ ocaml camlp5 findlib num coq ];
 
   preBuild = "coq_makefile -f _CoqProject -o Makefile";
 
